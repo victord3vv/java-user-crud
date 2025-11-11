48a0b04d239bc999c66a11fbee6253c60ce1a432
Vitinho <victorhugofake12@gmail.com>
2025-11-11 15:50:49 -0300

    feat: Implementa a camada UserService com todos os métodos CRUD

--- src/main/java/com/popezada/cadastro_usuario/business/UsuarioService.java
+++ src/main/java/com/popezada/cadastro_usuario/business/UsuarioService.java
@@ -17,9 +17,27 @@ public class UsuarioService {
         repository.saveAndFlush(usuario);
     }
 
-    public Usuario buscarUsuarioPoEmail(String email) {
+    public Usuario buscarUsuarioPorEmail(String email) {
         return repository.findByEmail(email).orElseThrow(
                 () -> new RuntimeException("Email não encontrado")
         );
+    }
+
+    public void deletarUsuarioPorEmail(String email) {
+        repository.deleteByEmail(email);
+    }
+
+    public void atualizarUsuarioPorId(Integer id, Usuario usuario) {
+        Usuario usuarioEntity = repository.findById(id).orElseThrow(
+                () -> new RuntimeException("Usuario não encontrado")
+        );
+        Usuario usuarioAtualizado = Usuario.builder()
+                .email(usuario.getEmail() != null ? usuario.getEmail() :
+                        usuarioEntity.getEmail())
+                .nome(usuario.getNome() != null ? usuario.getNome() :
+                        usuarioEntity.getNome())
+                .id(usuarioEntity.getId())
+                .build();
+        repository.saveAndFlush(usuarioAtualizado);
     }
 }
--- src/main/java/com/popezada/cadastro_usuario/infrastructure/repository/UsuarioRepository.java
+++ src/main/java/com/popezada/cadastro_usuario/infrastructure/repository/UsuarioRepository.java
@@ -1,6 +1,7 @@
 package com.popezada.cadastro_usuario.infrastructure.repository;
 
 import com.popezada.cadastro_usuario.infrastructure.entitys.Usuario;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -8,5 +9,9 @@ import java.util.Optional;
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 
     Optional<Usuario> findByEmail(String email);
+
+    @Transactional
+    void deleteByEmail(String email);
+
 
 }
